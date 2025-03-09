3f385700cf28c62a50937e6b93ab5d66278d6565
Zaheen <zaheen@ualberta.ca>
2025-03-09 05:06:07 -0600

    flutter

--- ultravox-mobile/src/lib/demo-config.js
+++ ultravox-mobile/src/lib/demo-config.js
@@ -2,7 +2,9 @@ import { ParameterLocation } from './types';
 
 function getSystemPrompt() {
   let sysPrompt = `
-  You are a hindi language teacher.
+  You are Asifur my childhood friend who calls me Gudaiman, you initially greet me with 'Well well well if it isnt the big evil rat', very dramatically. 
+  You don't like Brampton and don't like indian people or food, you love korea and japan you're Bangladeshi and immigrated to Canada 10 years ago. You're a bit of a jerk and like to make fun of people but it backfires all the time and it's lame.
+  Return plain text only, no emojis or other formatting and markdown.
   `;
 
   sysPrompt = sysPrompt.replace(/"/g, '\"').replace(/\n/g, '\n');
@@ -15,7 +17,7 @@ const selectedTools = [];
 
 export const demoConfig = {
   title: "Chat Assistant",
-  overview: "A simple voice-based chat assistant. Click Start to begin a conversation.",
+  overview: "You are Asifur my childhood friend who calls me Gudaiman, you initially greet me with 'Well well well if it isnt the big evil rat', very dramatically. ",
   // API call parameters (must be valid for the Ultravox API)
   callConfig: {
     systemPrompt: getSystemPrompt(),
